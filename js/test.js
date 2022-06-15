a27e9c8bc5f78f117ec47e98a47a13c9e0dc21ed
Vlad Shchelkunov <specter.ode@gmail.com>
2022-06-15 11:03:48 +0300

    1

--- js/test.js
+++ js/test.js
@@ -1,14 +1,76 @@
-function readMore() {
-    const dots = document.getElementById("dots");
-    const more = document.getElementById("more");
-    const btn = document.getElementById("btn");
-if(dots.style.display === "none") {
-    dots.style.display = "inline";
-    btn.innerHTML = "Read more";
-    more.style.display = "none";
-} else {
-    dots.style.display = "none";
-    btn.innerHTML = "Hide";
-    more.style.display = "inline";
-    }   
-}
+const users =
+[
+    {
+      name: "Moore Hensley",
+      email: "moorehensley@indexia.com",
+      eyeColor: "blue",
+      friends: ["Sharron Pace"],
+      isActive: false,
+      balance: 2811,
+      gender: "male"
+    },
+    {
+      name: "Sharlene Bush",
+      email: "sharlenebush@tubesys.com",
+      eyeColor: "blue",
+      friends: ["Briana Decker", "Sharron Pace"],
+      isActive: true,
+      balance: 3821,
+      gender: "female"
+    },
+    {
+      name: "Ross Vazquez",
+      email: "rossvazquez@xinware.com",
+      eyeColor: "green",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+      isActive: false,
+      balance: 3793,
+      gender: "male"
+    },
+    {
+      name: "Elma Head",
+      email: "elmahead@omatom.com",
+      eyeColor: "green",
+      friends: ["Goldie Gentry", "Aisha Tran"],
+      isActive: true,
+      balance: 2278,
+      gender: "female"
+    },
+    {
+      name: "Carey Barr",
+      email: "careybarr@nurali.com",
+      eyeColor: "blue",
+      friends: ["Jordan Sampson", "Eddie Strong"],
+      isActive: true,
+      balance: 3951,
+      gender: "male"
+    },
+    {
+      name: "Blackburn Dotson",
+      email: "blackburndotson@furnigeer.com",
+      eyeColor: "brown",
+      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      isActive: false,
+      balance: 1498,
+      gender: "male"
+    },
+    {
+      name: "Sheree Anthony",
+      email: "shereeanthony@kog.com",
+      eyeColor: "brown",
+      friends: ["Goldie Gentry", "Briana Decker"],
+      isActive: true,
+      balance: 2764,
+      gender: "female"
+    }
+  ]
+  let color = "blue";
+// const getUsersWithEyeColor = (users, color) => {
+    const getUsersWithEyeColor = (users, color) => {
+        const u = users.map(eyeColor => users.eyeColor)
+    users.flatMap(eyeColor => users.eyeColor).filter(c => c.eyeColor === color )
+    // };
+
+
+
+
