80e6c7923d098650b369862fee84467b55b632ae
Vlad Shchelkunov <specter.ode@gmail.com>
2022-06-22 10:43:17 +0300

    1

--- js/test.js
+++ js/test.js
@@ -1,31 +1,28 @@
-const players = [
-  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
-  { name: "Poly", playtime: 469, gamesPlayed: 2 },
-  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
-  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
-];
-// Change code below this line
-// const playtimes = players.map(player => player.playtime);
 
-// const gamesPlayed = players.map(player => player.gamesPlayed);
+class StringBuilder {
+  constructor(initialValue = '') {
+      this.value = initialValue;
+  }
+  getValue() {
+      return this.value;
+  }
+  padEnd(strEnd) {
+      return this.value = str;
+  }
+  padStart(strStart) {
+  return this.value.unshift(trStart);
+  }
+  padEnd(str) {
+  return this.value.push(str).unshift(str);
+}
+}
 
-const abc = Object.values(players)
-console.log(abc);
-const totalTime = players.reduce((total, player) => {
-  return total + player.playtime / player.gamesPlayed;
-}, 0);
-console.log(totalTime);
-const averagePlayTime = totalTime / players.length;
-console.log(averagePlayTime);
-const totalGamePlayed = players.reduce((total, player) => {
-  return total + player.gamesPlayed;
-}, 0);
-console.log(totalGamePlayed);
-
-const totalAveragePlaytimePerGame = totalTime / totalGamePlayed;
-console.log(totalAveragePlaytimePerGame);
-
-
-
-
-
+// Change code above this line
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
